d04c7e72a56a936526fbfdda308f0433e2fdc07f
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-02-13 14:20:30 -0600

    P data & useState complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList);
   
 
   return (
     <>
+    <div className="App">{puppies.map((puppy) => {
+      return <p>{puppy.name}</p>
+    })
+    }</div>
 
     </>
-  )
+  );
 }
 
 export default App
